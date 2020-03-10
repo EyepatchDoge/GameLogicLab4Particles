09d03662b7a3b43204fbdc38fc701813542d36f8
EyepatchDoge <kaspo.eyepatch@gmail.com>
2020-03-09 22:20:34 -0400

    implemented Raheel's package

---
+++ GameLogics2Lab4_ParticlesLab_KasCaron_301032846/Assets/Scripts/SpawnParticle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnParticle : MonoBehaviour
+{
+
+    public GameObject myFirstPrefab;
+    public GameObject mySecondPrefab;
+
+    public Camera myCamera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        {
+            Ray ray;
+            RaycastHit hit;
+            ray = myCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                GameObject myPrefab;
+                if (Input.GetMouseButtonDown(0))
+                {
+                    myPrefab = myFirstPrefab;
+                }
+                else
+                {
+                    myPrefab = mySecondPrefab;
+                }
+                Instantiate(myPrefab, hit.point, Quaternion.identity);
+            }
+        }
+    }
+}
